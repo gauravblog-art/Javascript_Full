5e6970fcc1e82f458ae736b103e5bee289ba70f6
Gaurav kumar Mishra <76816526+gauravblog-art@users.noreply.github.com>
2025-04-15 17:14:04 +0000

    naming and converstion

---
+++ 01_basic/04_conversion.js
@@ -0,0 +1,20 @@
+// let number = "23"
+let number = undefined
+
+
+let stringNumber = Number(number)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// "23" => 23 "23sba" => NaN (not a number)
+// true => 1 false => 0
+// null => 0  undefiend => NaN
+
+let stringN = 0
+
+let stringBool = Boolean(stringN)
+console.log(stringBool);
+console.log(typeof stringBool);
+
+// "" => false "gaurav" => true
+// 1 => true 0 => false
