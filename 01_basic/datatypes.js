1997515f4082be0dab68407f0f4a2f91041ae8a2
Gaurav kumar Mishra <76816526+gauravblog-art@users.noreply.github.com>
2025-04-15 16:49:44 +0000

    datatype done

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,16 @@
+// there are differant types of data type persent
+
+
+// string
+// undefined
+// bigint
+// boolean
+// null
+// symbol
+
+let state;
+
+console.log(state)
+console.log(typeof(null)) // return object
+console.log(typeof null)  // return object
+console.log(typeof undefined) // return undefined
